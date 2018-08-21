814f9d1c54e22bd087fc488e9391b1f15e86f963
alfredo tito <tito.alfred@hotmail.com>
2018-08-20 22:44:43 -0300

    primeiro passos

---
+++ training/first steps/main.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+    var width = canvas.width = window.innerWidth;
+    var height = canvas.height = window.innerHeight;
+
+    for(var i = 0; i < 200; i += 1) {
+        context.beginPath();
+        context.moveTo(Math.random() * width, Math.random() * height);
+        context.lineTo(Math.random() * width, Math.random() * height);
+        context.stroke();
+    }
+    
+};
